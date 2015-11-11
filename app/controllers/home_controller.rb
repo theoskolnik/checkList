a74342d3c64ff28fb743a1ea35d239d51c0d8ac1
Theo Skolnik <theodoraskolnik@gmail.com>
2015-11-11 10:40:02 -0600

    Adds new route for react code

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,7 @@ class HomeController < ApplicationController
 	def index
 		render :json => {name: (0...8).map { (65 + rand(26)).chr }.join }
 	end
+
+	def new
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   
   root 'home#index'
 
+  get '/new', :to => 'home#new', :as => 'new'
+
 end
